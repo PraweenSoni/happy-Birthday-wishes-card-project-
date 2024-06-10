b00fad5a5b23672cceea89df7ee8676387b61d07
akshatakka <akshatakka@users.noreply.github.com>
2024-06-10 13:06:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ const sname = document.getElementById("name")
 
 title.innerText = "My Love, My heart, My cutiepie<3";
 
-desc.innerText = I cannot tell you ho much I love you. These past 6 months have been the best 6 months of my life and you are the reason.I hope I never ever loose you and our bond grows stronger and deeper./";
+desc.innerText = "I cannot tell you ho much I love you. These past 6 months have been the best 6 months of my life and you are the reason.I hope I never ever loose you and our bond grows stronger and deeper.";
 
 sname.innerText = "Chokrii";
