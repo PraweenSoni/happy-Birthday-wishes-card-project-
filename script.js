b440b2c58b467e8f746ab62d228782318e1cd3a8
PraweenSoni <pramrajkumar1234@gmail.com>
2024-07-13 13:40:32 +0530

    Merge pinkerjit request

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ const sname = document.getElementById("name")
 
 title.innerText = "My Love, My heart, My cutiepie,My life <3";
 
-desc.innerText=" You are the love of my life and the reason for my happiness. On your birthday, I want to be your special for your life time,Happy birthday my gorgeous girl. Happy birthday to the sweetest girl in the world!"
-I am so lucky to have you in my life. and here's to many more together".
-Every day with you feels like an adventure.
-You're my sweetest dream come true. Happy cake day!"";
+desc.innerText=" You are the love of my life and the reason for my happiness. On your birthday, I want to be your special for your life time,Happy birthday my gorgeous girl. Happy birthday to the sweetest girl in the world! I am so lucky to have you in my life. and here's to many more together. Every day with you feels like an adventure. You're my sweetest dream come true. Happy cake day!";
 
 sname.innerText = "ushamba";
