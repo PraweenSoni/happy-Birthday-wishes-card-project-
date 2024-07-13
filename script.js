277a77f0475f9585be82ca2918cae37f95692e93
PraweenSoni <pramrajkumar1234@gmail.com>
2024-07-13 13:17:57 +0530

    Default

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+
+
+title.innerText = "Dear best friend,";
+
+desc.innerText = "Happy birthday!! I hope your day is filled with lots of love and laughter! May all of your birthday wishes come true.";
+
+sname.innerText = "Pks";
